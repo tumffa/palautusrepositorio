6e590f002a3f450657d1ae25c361135e62db53c8
tumffa <tuomas.v.niemi@gmail.com>
2023-12-11 15:11:32 +0200

    Tehtävä 3 + 4

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -1,6 +1,6 @@
 from statistics_1 import Statistics
 from player_reader import PlayerReader
-from matchers import And, HasAtLeast, PlaysIn, All, Not, HasFewerThan
+from matchers import And, HasAtLeast, PlaysIn, All, Not, HasFewerThan, Or, QueryBuilder
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2022-23/players.txt"
@@ -31,10 +31,32 @@ def main():
     Not(HasAtLeast(2, "goals")),
     PlaysIn("NYR")
     )
-    
+
     for player in stats.matches(matcher):
         print(player)
 
+    matcher = And(
+    HasAtLeast(70, "points"),
+    Or(
+        PlaysIn("NYR"),
+        PlaysIn("FLA"),
+        PlaysIn("BOS")
+        )
+    )   
+    print("-----------------")
+
+    query = QueryBuilder()
+
+    matcher = (
+        query
+        .playsIn("NYR")
+        .hasAtLeast(10, "goals")
+        .hasFewerThan(20, "goals")
+        .build()
+    )
+
+    for player in stats.matches(matcher):
+        print(player)
 
 if __name__ == "__main__":
     main()
--- viikko6/query-language/src/matchers.py
+++ viikko6/query-language/src/matchers.py
@@ -22,6 +22,17 @@ class Not:
         return True
 
 
+class Or:
+    def __init__(self, *matchers):
+        self._matchers = matchers
+
+    def test(self, player):
+        for matcher in self._matchers:
+            if matcher.test(player):
+                return True
+
+        return False
+
 class PlaysIn:
     def __init__(self, team):
         self._team = team
@@ -55,3 +66,21 @@ class HasFewerThan:
 class All:
     def test(self, player):
         return True
+    
+
+class QueryBuilder:
+    def __init__(self, build=None):
+        self._build = build if build else All()
+
+    def build(self):
+        return self._build
+    
+    def playsIn(self, team):
+        return QueryBuilder(And(self._build, PlaysIn(team)))
+    
+    def hasAtLeast(self, value, attr):
+        return QueryBuilder(And(self._build, HasAtLeast(value, attr)))
+    
+    def hasFewerThan(self, value, attr):
+        return QueryBuilder(And(self._build, HasFewerThan(value, attr)))
+    
